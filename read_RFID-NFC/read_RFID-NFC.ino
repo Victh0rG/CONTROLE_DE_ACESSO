e11a632646c5f79ac1c9c85613f2b1b6c5aa9cab
Victhor <victhorgb1080@gmail.com>
2024-05-07 00:54:39 -0300

    iniciando nfc PN532

--- read_RFID-NFC/read_RFID-NFC.ino
+++ read_RFID-NFC/read_RFID-NFC.ino
@@ -2,27 +2,26 @@
 //Autor: Victhor
 //Baseado no programa exemplo da biblioteca PN532 
 
-//Define a interface de comunicação
-//diretiva do pre-processador
+//Define qual a interface de comunicação por meio das diretivas de comilacao
 #if 0
 #include <SPI.h>  
-#include <PN532_SPI.h>  //Interface SPI
+#include <PN532_SPI.h> //Interface SPI
 #include "PN532.h"
 #include <SoftwareSerial.h>
-PN532_SPI pn532spi(SPI, 10);  //modulo de interface com PN532
+PN532_SPI pn532spi(SPI, 10); //modulo de interface com PN532
 PN532 nfc(pn532spi);
 
 #elif 1
 #include <PN532_HSU.h> //Interface HSU
 #include <PN532.h> 
 #include <SoftwareSerial.h> //
-SoftwareSerial SWSerial(10, 11);  //RX, TX
+SoftwareSerial SWSerial(10, 11); //RX, TX
 PN532_HSU pn532swhsu( SWSerial ); //modulo de interface com PN532
 PN532 nfc(pn532hsu);
 
 #else
 #include <Wire.h>
-#include <PN532_I2C.h>  //Interface I2C
+#include <PN532_I2C.h> //Interface I2C
 #include <PN532.h>
 #include <NfcAdapter.h>
 
@@ -32,5 +31,14 @@ PN532 nfc(pn532i2c);
 
 void setup(void)
 { 
-  
+  Serial.begin(115200);
+  Serial.println("Hello word");
+  nfc.begin(); //inicializacao do nfc
+  uint32_t versiondata = nfc.getFirmwareVersion();
+
+  if(! versiondata ){
+    Serial.print("Não econtrei o modulo PN532 :( ");
+    while(1); //para a execucao
+  }
+
 }
